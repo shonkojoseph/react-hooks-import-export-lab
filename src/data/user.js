44d9b3c8581f42374f1a578e16ce45e671c17e05
Joseph <josephshonko99@gmail.com>
2024-05-02 08:55:50 +0300

    commiting hooks

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,9 @@
-const username = "Liza";
-const city = "New York";
+export const username = "Liza";
+export const city = "New York";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+export const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+// export {name, city,image};
+// export {username, city, image};
